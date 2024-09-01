9d6889932a9e4133ea3701e68e11a03c2cc0a0b3
jbrylv <64449005+jbrylv@users.noreply.github.com>
2024-09-01 06:50:23 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+document.getElementById('form1').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const n = parseFloat(document.getElementById('n').value);
+    const d = parseInt(document.getElementById('d').value);
+    function calculateAQ(n, d) {
+        let AQ = [n];
+        let E = [n];
+
+        for (let i = 1; i <= d; i++) {
+            let lastAQ = AQ[i - 1];
+            let lastE = E[i - 1];
+
+            let newAQ = lastE - (lastE * 0.095);
+            let newE = (lastE + newAQ) / 2;
+
+            AQ.push(newAQ);
+            E.push(newE);
+        }
+
+        return AQ[d];
+    }
+
+    const result = calculateAQ(n, d);
+    document.getElementById('result').textContent = `LIQ: ${result.toFixed(4)}`;
+});
+
+
+
+
+document.getElementById('form2').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const FP = parseFloat(document.getElementById('fp').value);
+    const TC = parseInt(document.getElementById('tc').value);
+    function calculateTotal() {
+
+        let total = 0;
+
+        if (TC === 1 || TC === 2) {
+            total = FP * TC;
+        } else {
+            total = FP * (2 + 2 * (Math.pow(2, TC - 2) - 1));
+        }
+        
+        document.getElementById('result2').textContent = `Total: ${total.toFixed(2)}`;
+
+    }
+    calculateTotal();
+});
